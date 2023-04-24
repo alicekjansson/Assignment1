fea88eb8a9197cad5a1aa785c6e80b728d581938
alicekjansson <alice.jansson@iea.lth.se>
2023-04-24 11:04:23 +0200

    upload functions

--- Assignment1_Alice.py
+++ Assignment1_Alice.py
@@ -8,7 +8,7 @@ Created on Mon Apr 24 10:27:54 2023
 import sys
 sys.path.append(r'C:/Users/Alice/OneDrive - Lund University/Dokument/Doktorand IEA/Kurser/KTH kurs/pandapower-develop')
 
-import pandapower
+import pandapower as pp
 import pandapower.networks
 import pandapower.topology
 import pandapower.plotting
@@ -17,5 +17,13 @@ import pandapower.estimation
 
 import xml.etree.ElementTree as ET
 
+#Step 1: Parse XML files
 EQ=ET.parse('Assignment_EQ_reduced.xml') 
 grid=EQ.getroot()
+SSH=ET.parse('Assignment_SSH_reduced.xml') 
+loadflow=SSH.getroot()
+
+#Step 2: Create internal datastructures
+
+
+net=pp.create_empty_network()
---
+++ functions.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 24 11:03:44 2023
+
+@author: Alice
+"""
+
+#This file is for sharing functions which are of use to both of us
