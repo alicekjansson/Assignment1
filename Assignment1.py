9dfd0936ce3c1306f14cd4f905a9dc43ecdd2bbe
alicekjansson <128380748+alicekjansson@users.noreply.github.com>
2023-04-24 09:28:26 +0200

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 11 11:37:57 2023
+
+@author: Alice
+"""
+import sys
+sys.path.append(r'C:/Users/Alice/OneDrive - Lund University/Dokument/Doktorand IEA/Kurser/KTH kurs/pandapower-develop')
+
+import pandapower
+import pandapower.networks
+import pandapower.topology
+import pandapower.plotting
+import pandapower.converter
+import pandapower.estimation
+
