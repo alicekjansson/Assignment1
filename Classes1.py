261fda7dd4a6db349a9b1cf7870f180ccd1180f4
alicekjansson <alice.jansson@iea.lth.se>
2023-04-24 11:34:22 +0200

    Update Classes1.py

--- Classes1.py
+++ Classes1.py
@@ -4,31 +4,67 @@ Created on Mon Apr 24 11:09:05 2023
 
 @author: Alice
 """
-
+import pandas as pd
 import xml.etree.ElementTree as ET
 #Step 1: Parse XML files
 EQ=ET.parse('Assignment_EQ_reduced.xml') 
-grid=EQ.getroot()
 SSH=ET.parse('Assignment_SSH_reduced.xml') 
-loadflow=SSH.getroot()
+ns=ns = {'cim':'http://iec.ch/TC57/2013/CIM-schema-cim16#',
+      'entsoe':'http://entsoe.eu/CIM/SchemaExtension/3/1#',
+      'rdf':'{http://www.w3.org/1999/02/22-rdf-syntax-ns#}',
+      'md':"http://iec.ch/TC57/61970-552/ModelDescription/1#"}
 
 class Buses:
     
-    def __init__(self):
+    def __init__(self,eq,ssh,ns):
+        self.eq=eq
+        self.ssh=ssh
+        self.ns=ns
+        self.df=pd.DataFrame()
+        self.grid=EQ.getroot()
+        self.loadflow=SSH.getroot()
+        
+    def setup(self):
+        #Fill up dataframe based on XML files
         return
+
     
 class Transformers:
     
-    def __init__(self):
+    def __init__(self,eq,ssh):
+        self.eq=eq
+        self.ssh=ssh
+        self.df=pd.DataFrame()
+        self.grid=EQ.getroot()
+        self.loadflow=SSH.getroot()
+        
+    def setup(self):
+        #Fill up dataframe based on XML files
         return
 
 class Lines:
     
-    def __init__(self):
+    def __init__(self,eq,ssh):
+        self.eq=eq
+        self.ssh=ssh
+        self.df=pd.DataFrame()
+        self.grid=EQ.getroot()
+        self.loadflow=SSH.getroot()
+        
+    def setup(self):
+        #Fill up dataframe based on XML files
         return
     
 class Shunts:
     
-    def __init__(self):
+    def __init__(self,eq,ssh):
+        self.eq=eq
+        self.ssh=ssh
+        self.df=pd.DataFrame()
+        self.grid=EQ.getroot()
+        self.loadflow=SSH.getroot()
+        
+    def setup(self):
+        #Fill up dataframe based on XML files
         return
     
