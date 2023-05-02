d264d9abfd70dd3b0854f561261176b299d0a0a5
alicekjansson <alice.jansson@iea.lth.se>
2023-05-02 13:56:49 +0200

    Update Classes1.py

--- Classes1.py
+++ Classes1.py
@@ -9,8 +9,6 @@ import xml.etree.ElementTree as ET
 from functions import get_node
 
 
-
-
 class GridObjects:
     
     def __init__(self,eq,ssh,ns,element_type):
@@ -21,7 +19,7 @@ class GridObjects:
         self.ldf=ssh.getroot()
         self.df=pd.DataFrame()
         self.list=self.grid.findall('cim:'+element_type,ns)
-        self.df['ID']=[element.attrib.get(ns['rdf']+'ID') for element in self.list]
+        # self.df['ID']=[element.attrib.get(ns['rdf']+'ID') for element in self.list]
         
         
 class Buses(GridObjects):
@@ -53,9 +51,8 @@ class Buses(GridObjects):
             for terminal in self.grid.findall('cim:Terminal',ns):
                 if terminal.find('cim:Terminal.ConductingEquipment',ns).attrib.get(ns['rdf']+'resource') == "#" + id1:
                     connections[0].append(terminal.find('cim:IdentifiedObject.name',ns).text)
-                    for cn in self.grid.findall('cim:ConnectivityNode',ns):
-                        if terminal.find('cim:Terminal.ConnectivityNode',ns).attrib.get(ns['rdf']+'resource') == "#" + cn.attrib.get(ns['rdf']+'ID'):
-                            connections[1].append(cn.find('cim:IdentifiedObject.name',ns).text)
+                    connections[1].append(get_node(self.grid,terminal))
+                            
         #Add data to dataframe               
         self.df['Name']=names
         self.df['ipMax']=[(bus.find('cim:BusbarSection.ipMax',ns).text) for bus in self.bus_list]
@@ -74,9 +71,9 @@ class Transformers(GridObjects):
     def __init__(self, eq, ssh, ns, element_type = "PowerTransformer"):
         super().__init__(eq, ssh, ns, element_type)
         self.trans_list=self.grid.findall('cim:PowerTransformer',ns)
-        self.df=self.insert_busdata()
+        self.df=self.insert_transdata()
         
-    def insert_busdata(self):
+    def insert_transdata(self):
         names=[]
         subs=[]
         term=[[],[]]
@@ -95,9 +92,7 @@ class Transformers(GridObjects):
             #Add data about two sides of transformer
             i=0     #Keep track of which side of transformer
             for transend in self.grid.findall('cim:PowerTransformerEnd',ns):
-                
                 if '#' + transid == transend.find('cim:PowerTransformerEnd.PowerTransformer',ns).attrib.get(ns['rdf']+'resource'):
-                    idend=transend.attrib.get(ns['rdf']+'ID')   #ID of PowerTransformerEnd
                     rateds[i].append(transend.find('cim:PowerTransformerEnd.ratedS',ns).text)
                     ratedu[i].append(transend.find('cim:PowerTransformerEnd.ratedU',ns).text)
                     #Get data on terminals the transformer end is connected to
@@ -123,6 +118,50 @@ class Transformers(GridObjects):
     def get_df(self):
         return self.df
 
+class Lines(GridObjects):
+    
+    def __init__(self, eq, ssh, ns, element_type = "BusbarSection"):
+        super().__init__(eq, ssh, ns, element_type)
+        self.line_list=self.grid.findall('cim:ACLineSegment',ns)
+        self.df=self.insert_linedata()
+    
+    def insert_linedata(self):
+        names=[]
+        length=[]
+        volt=[]
+        term=[]
+        node=[]
+        pars=[[],[]]
+        for line in self.line_list:
+            lineid=line.attrib.get(ns['rdf']+'ID')  
+            name=line.find('cim:IdentifiedObject.name',ns)
+            names.append(name.text)
+            l=line.find('cim:Conductor.length',ns).text
+            length.append(l)
+            pars[0].append(float(line.find('cim:ACLineSegment.r',ns).text)/float(l))
+            pars[1].append(float(line.find('cim:ACLineSegment.x',ns).text)/float(l))
+            #Find voltage levels
+            for bv in self.grid.findall('cim:BaseVoltage',ns):
+                if line.find('cim:ConductingEquipment.BaseVoltage',ns).attrib.get(ns['rdf']+'resource') == '#' + bv.attrib.get(ns['rdf']+'ID'):
+                    volt.append(bv.find('cim:IdentifiedObject.name',ns).text)
+            #Find connected terminals
+            for terminal in self.grid.findall('cim:Terminal',ns):
+                if terminal.find('cim:Terminal.ConductingEquipment',ns).attrib.get(ns['rdf']+'resource') == "#" + lineid:
+                    term.append(terminal.find('cim:IdentifiedObject.name',ns).text)
+                    node.append(get_node(self.grid,terminal)) 
+        self.df['Name']=names
+        self.df['Length']=length
+        self.df['VoltageLevel']=volt
+        self.df['Terminal1']=term[::2]      #Every other item in list is for the first terminal
+        self.df['Terminal2']=term[1::2]     #Every other item in list is for the second terminal
+        self.df['Node1']=node[::2]
+        self.df['Node2']=node[1::2]
+        self.df['r/km']=pars[0]
+        self.df['x/km']=pars[1]
+        return self.df
+        
+    def get_df(self):
+        return self.df
 
 #Step 1: Parse XML files
 eq=ET.parse('Assignment_EQ_reduced.xml') 
@@ -137,6 +176,8 @@ buses=Buses(eq,ssh,ns)
 df_buses=buses.get_df()
 transformers=Transformers(eq,ssh,ns)
 df_trans=transformers.get_df()
+lines=Lines(eq,ssh,ns)
+df_lines=lines.get_df()
 
     
 
@@ -144,10 +185,7 @@ df_trans=transformers.get_df()
 
 
 
-class Lines(GridObjects):
-    
-    def __init__(self, eq, ssh, ns, element_type = "BusbarSection"):
-        super().__init__(eq, ssh, ns, element_type)
+
     
 #This is maybe??
 # class Shunts(GridObjects):
