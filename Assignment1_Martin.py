74af38155b933166f4a0daa8ef7f5d43982f16a6
MLundberg227 <79457915+MLundberg227@users.noreply.github.com>
2023-05-02 09:50:21 +0200

    Update Assignment1_Martin.py

--- Assignment1_Martin.py
+++ Assignment1_Martin.py
@@ -21,7 +21,7 @@ class GridNodeObjects:
         self.list=self.grid.findall('cim:'+element_type,ns)
         self.name = []
         self.connect = []
-        
+            
         
     def get_cim_connectivity(self):
         for item in self.list:
@@ -34,13 +34,35 @@ class GridNodeObjects:
         self.df['connection']=self.connect
         
         
+        
 class Buses(GridNodeObjects):
     
     def __init__(self, eq, ssh, ns, element_type = "BusbarSection"):
         super().__init__(eq, ssh, ns, element_type)
         
-    def create_pp_buses(net, self):
+    def get_cim_data(self):
+        self.voltage_lvl = []
         
+        for item in self.list:
+            container= item.find('cim:Equipment.EquipmentContainer',ns)
+            for vl in self.grid.findall('cim:VoltageLevel',ns):
+                if container.attrib.get(ns['rdf']+'resource') == "#" + vl.attrib.get(ns['rdf']+'ID'):
+                    self.voltage_lvl.append(vl.find('cim:IdentifiedObject.name',ns).text)
+        
+        self.df['voltage'] = self.voltage_lvl
+        
+
+
+        
+        
+    def create_pp_object(self, net):
+        bus_list = []
+       
+        bus_list.append(self.df.apply(lambda row: pp.create_bus(net, name=row['name'],vn_kv=row['voltage']),axis=1))
+        print(bus_list[0])
+       
+   
+
         
 
 class Loads(GridNodeObjects):
@@ -48,11 +70,20 @@ class Loads(GridNodeObjects):
     def __init__(self, eq, ssh, ns, element_type = "EnergyConsumer"):
         super().__init__(eq, ssh, ns, element_type)
         
-
+        '''
+    def setup_pp_loads(self, net, buses):
+        
+        self.df.apply(lambda row: create_pp(self, net, buses, row),axis=1)
+        
+    def create_pp(self, net, buses, load):
+        bus = buses.df[buses.df['connection'] == load['connection']]
+        print(bus['name'])
+        #self.df.apply(lambda row: pp.create_load(net, bus, name=row['name']),axis=1)
+'''
     
 class Generators(GridNodeObjects):
     
-    def __init__(self,eq,ssh,ns, element_type = "GeneratingUnit"):
+    def __init__(self,eq,ssh,ns, element_type = "SynchronousMachine"):
         super().__init__(eq, ssh, ns, element_type)
         
              
@@ -67,20 +98,24 @@ ns = {'cim':'http://iec.ch/TC57/2013/CIM-schema-cim16#',
 
 buses = Buses(eq,ssh,ns)
 loads = Loads(eq,ssh,ns)
-gens = Generators(eq,ssh,ns,"SynchronousMachine")
+gens = Generators(eq,ssh,ns)
 
 
 buses.get_cim_connectivity()
 loads.get_cim_connectivity()
 gens.get_cim_connectivity()
 
+buses.get_cim_data()
 
-print(buses.name)
+
+print(buses.df)
 print(loads.name)
 print(gens.name)
 
   
-net = pp.create_empty_network() 
+net = pp.create_empty_network()
+buses.create_pp_object(net)
+print(net.bus) 
 
 
 
